8acd6a9a10f4043501654644a8f4636a6dfd5e96
Korben Widmer <korbenwids@gmail.com>
2026-08-16 18:15:21 -0400

    Extract and export the GetTides handler and the getClosestStation function for testability

--- functions/src/functions/GetTides.js
+++ functions/src/functions/GetTides.js
@@ -13,117 +13,119 @@ const stations = stationsFilter.map(station => {
 });
 
 // this endpoint should be hit by GetCoords
-app.http('GetTides', {
-    methods: ['GET'],
-    handler: async (request, context) => {
-        context.log(`Processing http function request for url "${request.url}"`);
-        // get locaton from query parameter
-        const queryLocation = request.query.get('location');
+const handler = async (request, context) => {
+    context.log(`Processing http function request for url "${request.url}"`);
+    // get locaton from query parameter
+    const queryLocation = request.query.get('location');
 
-        const apiKey = process.env.GEO_API_KEY;
+    const apiKey = process.env.GEO_API_KEY;
 
-        // if query location is a falsey value
-        if (!queryLocation) {
-            context.log(`Query parameter 'location' is null. Received: ${queryLocation}`);
+    // if query location is a falsey value
+    if (!queryLocation) {
+        context.log(`Query parameter 'location' is null. Received: ${queryLocation}`);
+        return {
+            status: 400,
+            headers: {
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({
+                error: `Query parameter 'location' is null.`
+            })
+        };
+    }
+
+    let geocodeData;
+
+    // fetch the geocode api
+    try {
+        const geocodeApiUrl = `https://geocode.maps.co/search?q=${encodeURIComponent(queryLocation)}&countrycodes=us`;
+        const geocodeRes =  await fetch(geocodeApiUrl, {
+            headers: {
+                'Authorization': `Bearer ${apiKey}` // add API key in the headers for security
+            }
+        });
+
+        // throw an error if there is a bad response
+        if (!geocodeRes.ok) {
+            throw new Error(`API responded with status: ${geocodeRes.status}`);
+        }
+
+        geocodeData = await geocodeRes.json();
+        geocodeData = geocodeData[0];
+
+        // if data is null return 404
+        if (!geocodeData) {
             return {
-                status: 400,
+                status: 404,
                 headers: {
                     'Content-Type' : 'application/json'
                 },
                 body: JSON.stringify({
-                    error: `Query parameter 'location' is null.`
+                    error: 'No results found'
                 })
             };
         }
-
-        let geocodeData;
-
-        // fetch the geocode api
-        try {
-            const geocodeApiUrl = `https://geocode.maps.co/search?q=${encodeURIComponent(queryLocation)}&countrycodes=us`;
-            const geocodeRes =  await fetch(geocodeApiUrl, {
-                headers: {
-                    'Authorization': `Bearer ${apiKey}` // add API key in the headers for security
-                }
-            });
-
-            // throw an error if there is a bad response
-            if (!geocodeRes.ok) {
-                throw new Error(`API responded with status: ${geocodeRes.status}`);
-            }
-
-            geocodeData = await geocodeRes.json();
-            geocodeData = geocodeData[0];
-
-            // if data is null return 404
-            if (!geocodeData) {
-                return {
-                    status: 404,
-                    headers: {
-                        'Content-Type' : 'application/json'
-                    },
-                    body: JSON.stringify({
-                        error: 'No results found'
-                    })
-                };
-            }
-        } catch (err) {
-            return {
-                status: 502,
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    error: "Failed to fetch from geocode.maps.co",
-                    details: err.message
-                })
-            };
-        }
-
-        // fetch the NOAA tides api
-        try {
-            const closestStationId = getClosestStation({lat: geocodeData.lat, lng: geocodeData.lon}).id;
-
-            const tidesApiUrl = `https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?station=${closestStationId}&product=predictions&date=today&datum=MLLW&units=metric&time_zone=lst_ldt&format=json`;
-            const tidesRes = await fetch(tidesApiUrl);
-
-            if (!tidesRes.ok) {
-                throw new Error(`API responded with status: ${tidesRes.status}`);
-            }
-
-            const tidesData = await tidesRes.json();
-
-            // create response object
-            const responseJson = {
-                name: geocodeData.name,
-                displayName: geocodeData.display_name,
-                address: geocodeData.address,
-                predictions: tidesData.predictions,
-            };
-
-            return {
-                status: 200,
-                headers: { 'Content-Type': 'application/json'},
-                body: JSON.stringify(responseJson)
-            }
-        } catch (err) {
-            return {
-                status: 502,
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({
-                    error: "Failed to fetch from tides.noaa.gov",
-                    details: err.message
-                })
-            };
-        }
+    } catch (err) {
+        return {
+            status: 502,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                error: "Failed to fetch from geocode.maps.co",
+                details: err.message
+            })
+        };
     }
+
+    // fetch the NOAA tides api
+    try {
+        const closestStationId = getClosestStation({lat: geocodeData.lat, lng: geocodeData.lon}, stations).id;
+
+        const tidesApiUrl = `https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?station=${closestStationId}&product=predictions&date=today&datum=MLLW&units=metric&time_zone=lst_ldt&format=json`;
+        const tidesRes = await fetch(tidesApiUrl);
+
+        if (!tidesRes.ok) {
+            throw new Error(`API responded with status: ${tidesRes.status}`);
+        }
+
+        const tidesData = await tidesRes.json();
+
+        // create response object
+        const responseJson = {
+            name: geocodeData.name,
+            displayName: geocodeData.display_name,
+            address: geocodeData.address,
+            predictions: tidesData.predictions,
+        };
+
+        return {
+            status: 200,
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify(responseJson)
+        }
+    } catch (err) {
+        return {
+            status: 502,
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                error: "Failed to fetch from tides.noaa.gov",
+                details: err.message
+            })
+        };
+    }
+};
+
+app.http('GetTides', {
+    methods: ['GET'],
+    handler
 });
 
 // function to calculate the closest station to the input coordinates
-function getClosestStation(coords) {
+function getClosestStation(coords, stationList) {
     function calcDistance(lat1, lon1, lat2, lon2) {
         return Math.sqrt((lat2 - lat1)**2 + (lon2 - lon1)**2);
     }
 
-    const station = stations.reduce((prev, curr) => {
+    const station = stationList.reduce((prev, curr) => {
         const currDistance = calcDistance(coords.lat, coords.lng, curr.lat, curr.lng);
         const prevDistance = calcDistance(coords.lat, coords.lng, prev.lat, prev.lng);
 
@@ -136,3 +138,5 @@ function getClosestStation(coords) {
 
     return station;
 }
+
+module.exports = { handler, getClosestStation };
